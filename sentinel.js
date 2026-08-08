6258282d0f431dfdd6d494498115dd6ca930e434
SpartanKing18 <jawahar6k@gmail.com>
2026-08-08 14:09:04 -0700

    Release v2.23.0

--- sentinel.js
+++ sentinel.js
@@ -15,7 +15,7 @@ const crypto = require("crypto");
 const readline = require("readline");
 const os = require("os");
 const dnsp = require("dns").promises;
-const VERSION = "2.22.0";
+const VERSION = "2.23.0";
 
 // ---------- colors ----------
 const useColor = process.stdout.isTTY && !process.env.NO_COLOR;
